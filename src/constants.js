9bc29098106da557195fe5fcc5666d3d8df1c290
Rohan Maharjan <rohanmrzan100@gmail.com>
2025-11-17 21:43:14 +0545

    add bakcendf

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 let BACKEND_URL = 'http://localhost:3001';
 
 if (import.meta.env.MODE == 'production') {
-  BACKEND_URL = '';
+  BACKEND_URL = 'http://202.51.82.227:3001';
 }
 export { BACKEND_URL };
